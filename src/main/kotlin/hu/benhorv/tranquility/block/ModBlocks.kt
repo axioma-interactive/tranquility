3077ed6134c2cf0d177d32e54064932e0a4137a1
Hopper8338 <hopper8338@gmail.com>
2025-09-14 13:17:28 +0200

    add masonic cobblestone

--- src/main/kotlin/hu/benhorv/tranquility/block/ModBlocks.kt
+++ src/main/kotlin/hu/benhorv/tranquility/block/ModBlocks.kt
@@ -18,6 +18,16 @@ import net.minecraft.util.Identifier
 
 object ModBlocks {
 
+    //tranquility mod cuccok
+
+    val MASONIC_COBBLESTONE: Block = registerBlock("masonic_cobblestone", AbstractBlock.Settings.create()
+        .strength(4f)
+        .requiresTool()
+        .sounds(BlockSoundGroup.STONE)
+    )
+
+    //Kobanyai mod cuccok
+
     val ALUMINIUM_BLOCK: Block = registerBlock("aluminium_block", AbstractBlock.Settings.create()
         .strength(4f)
         .requiresTool()
