a6c838f2926ba757ccb07975692a603f2220ad23
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-09-14 14:54:22 +0200

    add tranquility item group
    # Conflicts:
    #	src/main/resources/assets/tranquility/lang/en_us.json

---
+++ src/main/kotlin/hu/benhorv/tranquility/item/ModItemGroups.kt
@@ -0,0 +1,29 @@
+package hu.benhorv.tranquility.item
+
+import hu.benhorv.tranquility.Tranquility
+import hu.benhorv.tranquility.Tranquility.logger
+import hu.benhorv.tranquility.block.ModBlocks
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+
+object ModItemGroups {
+    val TRANQUILITY_ITEMS_GROUP : ItemGroup = Registry.register(Registries.ITEM_GROUP,
+        Identifier.of(Tranquility.MOD_ID, "tranquility_items"),
+        FabricItemGroup.builder().icon( { ItemStack(ModItems.RAW_ALUMINIUM) } )
+            .displayName(Text.translatable("itemgroup.tranquility.tranquility_items"))
+            .entries {
+                displayContext, entries ->
+                entries.add(ModBlocks.ALUMINIUM_BLOCK)
+                entries.add(ModBlocks.RAW_ALUMINIUM_BLOCK)
+                entries.add(ModBlocks.ALUMINIUM_ORE)
+            }.build())
+
+    fun registerItemGroups() {
+        logger.info("Registering Item Groups for " + Tranquility.MOD_ID)
+    }
+}
