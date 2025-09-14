49b4fd6368c455f656c90d60fbd116bd22ea3978
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-09-14 10:50:50 +0200

    initial commit

---
+++ src/main/kotlin/hu/benhorv/fasom/FasomClient.kt
@@ -0,0 +1,10 @@
+package hu.benhorv.fasom
+
+import hu.benhorv.fasom.item.ModItems
+import net.fabricmc.api.ClientModInitializer
+
+object FasomClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
---
+++ src/main/kotlin/hu/benhorv/fasom/FasomDataGenerator.kt
@@ -0,0 +1,9 @@
+package hu.benhorv.fasom
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object FasomDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
