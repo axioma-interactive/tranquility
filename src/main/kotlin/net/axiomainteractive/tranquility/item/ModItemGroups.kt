03ee46771743edc9d4c22ded9a5489ffd87bea2b
Hopper8338 <hopper8338@gmail.com>
2025-12-21 15:47:39 +0100

    add compact mossy masonic cobblestone

--- src/main/kotlin/net/axiomainteractive/tranquility/item/ModItemGroups.kt
+++ src/main/kotlin/net/axiomainteractive/tranquility/item/ModItemGroups.kt
@@ -21,6 +21,7 @@ object ModItemGroups {
                 entries.add(ModBlocks.MASONIC_STONE)
                 entries.add(ModBlocks.MASONIC_COBBLESTONE)
                 entries.add(ModBlocks.MOSSY_MASONIC_COBBLESTONE)
+                entries.add(ModBlocks.COMPACT_MOSSY_MASONIC_COBBLESTONE)
                 entries.add(ModBlocks.CRIMSON_OBSIDIAN)
                 entries.add(ModBlocks.ALUMINIUM_ORE)
                 entries.add(ModBlocks.DEEPSLATE_ALUMINIUM_ORE)
