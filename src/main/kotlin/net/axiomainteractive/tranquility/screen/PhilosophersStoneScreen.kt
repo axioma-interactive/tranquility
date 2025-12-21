d1c097a4796f887496ec7aa222273732fd9e3fc1
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-12-21 18:22:12 +0100

    feat: Add custom textures for the Philosopher's Stone block and its GUI, and adjust GUI slot positions.

--- src/main/kotlin/net/axiomainteractive/tranquility/screen/PhilosophersStoneScreen.kt
+++ src/main/kotlin/net/axiomainteractive/tranquility/screen/PhilosophersStoneScreen.kt
@@ -1,6 +1,6 @@
 package net.axiomainteractive.tranquility.screen
 
-import com.mojang.blaze3d.systems.RenderSystem
+import net.minecraft.client.gl.RenderPipelines
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.entity.player.PlayerInventory
@@ -9,20 +9,17 @@ import net.minecraft.util.Identifier
 
 class PhilosophersStoneScreen(handler: PhilosophersStoneScreenHandler, inventory: PlayerInventory, title: Text) : HandledScreen<PhilosophersStoneScreenHandler>(handler, inventory, title) {
     
-    private val TEXTURE = Identifier.of("minecraft", "textures/gui/container/dispenser.png")
+    private val TEXTURE = Identifier.of("tranquility", "textures/gui/container/philosophers_stone_gui.png")
 
     override fun init() {
         super.init()
         titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2
     }
 
-
-
     override fun drawBackground(context: DrawContext, delta: Float, mouseX: Int, mouseY: Int) {
         val x = (width - backgroundWidth) / 2
         val y = (height - backgroundHeight) / 2
-        // Fallback to solid color as RenderPipeline API is unstable/unresolved
-        context.fill(x, y, x + backgroundWidth, y + backgroundHeight, -0x39393a) // Dark grey
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, x, y, 0f, 0f, backgroundWidth, backgroundHeight, 256, 256)
     }
 
     override fun drawForeground(context: DrawContext, mouseX: Int, mouseY: Int) {
